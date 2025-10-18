e3d1197f97296dc4dcfd92e1d9fa147bbffa9196
yahyamassad <yahyamassad@gmail.com>
2025-10-18 12:01:40 +0300

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -127,3 +127,9 @@ export const ChevronDownIcon: React.FC<{ className?: string }> = ({ className })
     <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
   </svg>
 );
+
+export const GlobeIcon: React.FC<{ className?: string }> = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${className}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m0 0a9 9 0 019-9m-9 9a9 9 0 009 9" />
+  </svg>
+);
