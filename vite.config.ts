f39cd59c8cf052a8e7df2ea2bf2678cf5596e034
yahyamassad <yahyamassad@gmail.com>
2025-11-03 00:45:08 +0300

    Update vite.config.ts
    اصلاح  خطأ في النشر

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Expose the Vercel-provided environment variable to the client-side code.
+    // Vite performs a direct string replacement, so the value needs to be stringified.
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    // FIX: Expose Firebase environment variables to the client-side code. This is necessary
+    // to make them available under `process.env` and resolve TypeScript errors.
+    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+    'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
+    'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
+    'process.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
+    'process.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+    'process.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
+    'process.env.VITE_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.VITE_FIREBASE_MEASUREMENT_ID),
+  }
 })
