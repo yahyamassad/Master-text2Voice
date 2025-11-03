1983b4c96c7cfda5eb11ef2109f02b5646149565
yahyamassad <yahyamassad@gmail.com>
2025-11-03 14:37:48 +0300

    Update icons.tsx
    تغيير تعريف الايكون - لاصلاح نشر جديد

--- components/icons.tsx
+++ components/icons.tsx
@@ -6,9 +6,12 @@ interface IconProps {
 }
 
 // A helper function to safely combine class names.
-const cn = (...classes: (string | undefined)[]) => classes.filter(Boolean).join(' ');
+function cn(...classes: (string | undefined)[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
-export const SpeakerIcon = () => (
+export function SpeakerIcon() {
+  return (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="h-6 w-6"
@@ -23,9 +26,11 @@ export const SpeakerIcon = () => (
       d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
     />
   </svg>
-);
+  );
+}
 
-export const SoundWaveIcon = () => (
+export function SoundWaveIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -47,10 +52,12 @@ export const SoundWaveIcon = () => (
         <rect className="wave-bar" x="14" y="4" width="2" height="16" rx="1" transform-origin="center" />
         <rect className="wave-bar" x="19" y="4" width="2" height="16" rx="1" transform-origin="center" />
     </svg>
-);
+    );
+}
 
 
-export const LoaderIcon = () => (
+export function LoaderIcon() {
+  return (
   <svg
     className="animate-spin h-6 w-6 text-white"
     xmlns="http://www.w3.org/2000/svg"
@@ -71,9 +78,11 @@ export const LoaderIcon = () => (
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     ></path>
   </svg>
-);
+  );
+}
 
-export const DownloadIcon = () => (
+export function DownloadIcon() {
+  return (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     className="h-6 w-6" 
@@ -88,9 +97,11 @@ export const DownloadIcon = () => (
       d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" 
     />
   </svg>
-);
+  );
+}
 
-export const TranslateIcon = () => (
+export function TranslateIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -105,9 +116,11 @@ export const TranslateIcon = () => (
             d="M3 5h12M9 3v2m4 13-4-4m0 0-4 4m4-4v12M21 12a9 9 0 11-18 0 9 9 0 0118 0z" 
         />
     </svg>
-);
+    );
+}
 
-export const StopIcon = () => (
+export function StopIcon() {
+  return (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="h-6 w-6"
@@ -120,15 +133,19 @@ export const StopIcon = () => (
       clipRule="evenodd"
     />
   </svg>
-);
+  );
+}
 
-export const PauseIcon = () => (
+export function PauseIcon() {
+    return (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
         <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
     </svg>
-);
+    );
+}
 
-export const StarIcon = ({ className }: IconProps) => (
+export function StarIcon({ className }: IconProps) {
+  return (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     className={cn('h-5 w-5', className)}
@@ -137,39 +154,51 @@ export const StarIcon = ({ className }: IconProps) => (
   >
     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
   </svg>
-);
+  );
+}
 
-export const CopyIcon = ({ className }: IconProps) => (
+export function CopyIcon({ className }: IconProps) {
+  return (
   <svg xmlns="http://www.w3.org/2000/svg" className={cn('h-5 w-5', className)} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
   </svg>
-);
+  );
+}
 
-export const CheckIcon = ({ className }: IconProps) => (
+export function CheckIcon({ className }: IconProps) {
+  return (
   <svg xmlns="http://www.w3.org/2000/svg" className={cn('h-5 w-5', className)} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
   </svg>
-);
+  );
+}
 
-export const ExternalLinkIcon = ({ className }: IconProps) => (
+export function ExternalLinkIcon({ className }: IconProps) {
+  return (
   <svg xmlns="http://www.w3.org/2000/svg" className={cn('h-4 w-4 inline-block mx-1', className)} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
   </svg>
-);
+  );
+}
 
-export const ChevronDownIcon = ({ className }: IconProps) => (
+export function ChevronDownIcon({ className }: IconProps) {
+  return (
   <svg xmlns="http://www.w3.org/2000/svg" className={cn('h-5 w-5', className)} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
   </svg>
-);
+  );
+}
 
-export const GlobeIcon = ({ className }: IconProps) => (
+export function GlobeIcon({ className }: IconProps) {
+  return (
   <svg xmlns="http://www.w3.org/2000/svg" className={cn('h-5 w-5', className)} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m0 0a9 9 0 019-9m-9 9a9 9 0 009 9" />
   </svg>
-);
+  );
+}
 
-export const ReplayIcon = () => (
+export function ReplayIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -184,9 +213,11 @@ export const ReplayIcon = () => (
             d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z" 
         />
     </svg>
-);
+    );
+}
 
-export const SwapIcon = () => (
+export function SwapIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -201,16 +232,20 @@ export const SwapIcon = () => (
             d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" 
         />
     </svg>
-);
+    );
+}
 
-export const MicrophoneIcon = ({ className }: IconProps) => (
+export function MicrophoneIcon({ className }: IconProps) {
+  return (
   <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="currentColor">
     <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z" />
     <path d="M17 11h-1c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92z" />
   </svg>
-);
+  );
+}
 
-export const GearIcon = () => (
+export function GearIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -230,9 +265,11 @@ export const GearIcon = () => (
             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" 
         />
     </svg>
-);
+    );
+}
 
-export const HistoryIcon = () => (
+export function HistoryIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -247,9 +284,11 @@ export const HistoryIcon = () => (
             d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" 
         />
     </svg>
-);
+    );
+}
 
-export const LinkIcon = () => (
+export function LinkIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -264,9 +303,11 @@ export const LinkIcon = () => (
             d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" 
         />
     </svg>
-);
+    );
+}
 
-export const ShareIcon = () => (
+export function ShareIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -281,15 +322,19 @@ export const ShareIcon = () => (
             d="M8.684 13.342C8.886 12.938 9 12.482 9 12s-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.368a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z" 
         />
     </svg>
-);
+    );
+}
 
-export const InfoIcon = ({ className }: IconProps) => (
+export function InfoIcon({ className }: IconProps) {
+    return (
     <svg xmlns="http://www.w3.org/2000/svg" className={cn('h-5 w-5', className)} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-);
+    );
+}
 
-export const PlayCircleIcon = () => (
+export function PlayCircleIcon() {
+    return (
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         className="h-6 w-6" 
@@ -298,9 +343,11 @@ export const PlayCircleIcon = () => (
     >
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v8a1 1 0 001.555.832l6-4a1 1 0 000-1.664l-6-4z" clipRule="evenodd" />
     </svg>
-);
+    );
+}
 
-export const SawtliLogoIcon = ({ className }: IconProps) => (
+export function SawtliLogoIcon({ className }: IconProps) {
+    return (
     <svg 
         viewBox="0 0 70 32" 
         fill="none" 
@@ -328,4 +375,5 @@ export const SawtliLogoIcon = ({ className }: IconProps) => (
             <rect x="66" y="9" width="4" height="14" rx="2"/>
         </g>
     </svg>
-);
+    );
+}
