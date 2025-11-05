9cf88c62e5b91c2cc23d9e8b52e1dd44776f2381
yahyamassad <yahyamassad@gmail.com>
2025-11-05 17:07:44 +0300

    Create types.ts
    ملف جديد

---
+++ Master-text2Voice/types.ts
@@ -0,0 +1,13 @@
+export interface HistoryItem {
+  id: string;
+  sourceText: string;
+  translatedText: string;
+  sourceLang: string;
+  targetLang: string;
+  timestamp: number;
+}
+
+export interface SpeakerConfig {
+    name: string;
+    voice: string;
+}
