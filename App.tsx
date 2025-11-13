dd220acd0c7939fca2de339a2b0db721edc20b1e
yahyamassad <yahyamassad@gmail.com>
2025-11-13 18:35:55 +0300

    Update App.tsx اضافة سطرين من عمو سطر 18-19

--- App.tsx
+++ App.tsx
@@ -15,7 +15,8 @@ import { generateSpeech, translateText } from './services/geminiService';
 import { playAudio, createWavBlob, createMp3Blob } from './utils/audioUtils';
 import { subscribeToHistory, addHistoryItem, clearHistoryForUser, deleteUserDocument } from './services/firestoreService';
 import { t, Language, languageOptions, translationLanguages, LanguageListItem } from './i18n/translations';
-import { isFirebaseConfigured, auth } from './firebaseConfig';
+import { getFirebase } from './firebaseConfig';
+const { isFirebaseConfigured, auth } = getFirebase();
 // Fix: Use Firebase v8 imports and types to match project dependencies
 // FIX: Use compat libraries for Firebase v9 with v8 syntax.
 import firebase from 'firebase/compat/app';
